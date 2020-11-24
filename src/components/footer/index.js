4247f14d7ee3e25166c944bb7126c607db40fad7
Chike <hyginuschinwoke@gmail.com>
2020-11-24 18:35:59 +0100

    add and style footer section

---
+++ src/components/footer/index.js
@@ -0,0 +1,29 @@
+import { Container, Row, Column, Link, Title, Text, Break } from './styles';
+
+const Footer = ({ children, ...restProps }) => (
+  <Container {...restProps}>{children}</Container>
+);
+
+Footer.Row = ({ children, ...restProps }) => (
+  <Row {...restProps}>{children}</Row>
+);
+
+Footer.Column = ({ children, ...restProps }) => (
+  <Column {...restProps}>{children}</Column>
+);
+
+Footer.Link = ({ children, ...restProps }) => (
+  <Link {...restProps}>{children}</Link>
+);
+
+Footer.Title = ({ children, ...restProps }) => (
+  <Title {...restProps}>{children}</Title>
+);
+
+Footer.Text = ({ children, ...restProps }) => (
+  <Text {...restProps}>{children}</Text>
+);
+
+Footer.Break = ({ ...restProps }) => <Break {...restProps} />;
+
+export default Footer;
